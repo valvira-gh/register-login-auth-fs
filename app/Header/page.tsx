d49ec747342e7609d86d7101601765484cae89a7
virvaldev <valtteri.virtanen.dev@gmail.com>
2024-01-01 22:09:24 +0200

    Some initial work.

---
+++ app/Header/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./page.module.css";
+
+const Header: React.FC = () => {
+
+    return (
+        <header className={styles.header}>
+            <h1>The Main Title and Header</h1>
+        </header>
+    )
+    )
+}
